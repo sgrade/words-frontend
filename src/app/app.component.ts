b330c8725dbd59789fcc1256f4dc0da4e355463e
Roman Klyuev <sgrade@users.noreply.github.com>
2020-02-04 16:52:27 +0100

    Active nav tab is properly selected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthProvider } from '@firebaseui/ng-bootstrap';
 
-import { Input } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-root',
@@ -11,8 +10,25 @@ import { Input } from '@angular/core';
 })
 
 export class AppComponent {
-	title = 'Learning Words';
-	activeMenuTab = 'None';
-	providers = AuthProvider;
+  title = 'Learning Words';
+  providers = AuthProvider;
+  activeMenuTab;
+
+  links = [
+    { title: 'Play', fragment: 'dashboard' },
+    { title: 'Manage words', fragment: 'words' },
+    { title: 'Login', fragment: 'auth' }
+  ];
+  constructor(public route: ActivatedRoute) {
+
+    // If I manually type non-default url in the browser's address bar, e.g. ../words, then the first nav tab still active
+    // To choose current active router tab, the following is used in the template
+    // routerLinkActive="active"
+    // However, ngbNav still chooses first tab as default, so two tabs are selected as active 
+    // (one by the router and one by ngbNav). 
+    // To help ngbNav choose proper tab, the following is added 
+    this.activeMenuTab = route.outlet;
+  }
+
 }
 
