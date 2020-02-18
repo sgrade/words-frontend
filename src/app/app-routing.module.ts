28f7d330a4f1f3f690596488bf08a0188497f56c
Roman Klyuev <sgrade@users.noreply.github.com>
2020-02-18 14:48:51 +0100

    mat-dashboard works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { DashboardComponent } from './dashboard/dashboard.component';
+// Old dashboard - saved for reference
+// import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatDashboardComponent } from './mat-dashboard/mat-dashboard.component';
 import { WordsComponent } from './words/words.component';
 import { WordDetailComponent } from './word-detail/word-detail.component';
 import { NgxAuthFirebaseuiComponent } from './ngx-auth-firebaseui/ngx-auth-firebaseui.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent},
+  { path: 'dashboard', component: MatDashboardComponent},
   { path: 'detail/:id', component: WordDetailComponent},
   { path: 'words', component: WordsComponent },
   { path: 'signin', component: NgxAuthFirebaseuiComponent },
